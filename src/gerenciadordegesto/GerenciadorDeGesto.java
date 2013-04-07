ba531de8542079c1fae18fb13ac8028eef550466
JB <>
2013-04-07 16:10:19 -0300

    Detecção de Movimentos Lançando eventos

--- src/gerenciadordegesto/GerenciadorDeGesto.java
+++ src/gerenciadordegesto/GerenciadorDeGesto.java
@@ -14,6 +14,6 @@ public class GerenciadorDeGesto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("eu");
     }
 }
