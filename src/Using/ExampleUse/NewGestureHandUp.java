294a149de2a8542fce2f2d47efbeef4e76cfc291
JB <>
2013-05-03 11:04:19 -0300

    Controlling Equipment with help kinect.

--- src/Using/ExampleUse/NewGestureHandUp.java
+++ src/Using/ExampleUse/NewGestureHandUp.java
@@ -16,6 +16,8 @@ public class NewGestureHandUp extends Gesture {
 
     //Within the class 'Gesture' has a vector points 'point' six 3D positions is used as starting points automatically read by the library.
     //The six 3D points passed as parameters to the method are possible together with several tests. The method that will insert the values ​​of each point and each joint will be called the point
+    //If you must use the initial points of any board, which lie within a vector (point) and can be accessed by 'GetPoint (i)' passing as a parameter the position vector to use.
+    //To use this vector is necessary to call the method 'setStartingPoints (boolean)' and pass true as a parameter
     @Override
     public boolean happened(Point3D point1, Point3D point2, Point3D point3, Point3D point4, Point3D point5, Point3D point6) {
         //Creating variable to return
--- src/Using/ExemploUso/GestoMaoParaCima.java
+++ src/Using/ExemploUso/GestoMaoParaCima.java
@@ -11,17 +11,17 @@ import org.OpenNI.Point3D;
 public class GestoMaoParaCima extends Gesture {
     //Dentro da classe 'Gesture' tem um vetor de pontos 'point' de seis posições em 3D que é usado como pontos iniciais, automaticamente lidos pela biblioteca.
     //Os seis pontos em 3D passados como parâmetros no método são para possíveis testes com várias juntas. O método que vai inserir os valores de cada ponto e cada junta vai ser chamado no point   
+    //Caso precise usar os pontos iniciais de alguma junta, que se encontram dentro de um vetor(point), podendo ser acessado pelo 'getPoint(i)' passando como parâmetro a posição do vetor que deseja usar.
+    //Para o uso desse vetor é necessário chamar o método 'setStartingPoints(boolean)' e passar true como parâmetro
 
     @Override
     public boolean happened(Point3D ponto1, Point3D ponto2, Point3D ponto3, Point3D ponto4, Point3D ponto5, Point3D ponto6) {
         //Criando variável para retorno
         boolean aconteceu = false;
         //Condição para  que o seu gesto aconteça
-        
-            if (ponto1.getY()<ponto2.getY()) {
-                aconteceu = true;
-            }
-        
+        if (ponto1.getY() < ponto2.getY()) {
+            aconteceu = true;
+        }
         //Retornando valor
         return aconteceu;
     }
